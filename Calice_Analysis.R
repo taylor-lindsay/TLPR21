785ec28572b56171867bc7102d7a7fb36eb01fd8
taylor-lindsay <taylor_lindsay@uri.edu>
2022-08-23 10:31:24 -0400

    updaing labels that are wrong

--- Calice_Analysis.R
+++ Calice_Analysis.R
@@ -4,28 +4,42 @@
 library(tidyverse)
 library(ggplot2)
 
-
 # Import data 
-raw_calice <- read.csv('~/Desktop/GITHUB/TLPR21/Transplants_Calice_Master.csv')
+raw_calice <- read.csv('~/Desktop/GITHUB/CF_2022/Transplants_Calice_Master.csv')
 
 #separate info from sample ID
 raw_calice <- separate(raw_calice, Label, into= c("species","treatment", "sample", "replicate"), sep = "_")
 
 # Merge multiple calices per sample 
 
-calice_W_means <- raw_calice %>%
+calice_means <- raw_calice %>%
   #filter(MEASUREMENT == "W") %>%
   group_by(species, treatment, sample, replicate, MEASUREMENT) %>%
-  summarize(., mean_W = mean(Length))
+  summarize(., mean_length = mean(Length))
+
+calice_area_means <- raw_calice %>%
+  #filter(MEASUREMENT == "W") %>%
+  group_by(species, treatment, sample, replicate, MEASUREMENT) %>%
+  summarize(., mean_area = mean(Area)) %>%
+  .[(.$MEASUREMENT=="CA"),] %>%
+  .[!(.$species=="IMG"),]
+
+calice_W_means <-  calice_means[(calice_means$MEASUREMENT=="W"),] %>%
+  .[!(.$species=="IMG"),]
+calice_H_means <-  calice_means[(calice_means$MEASUREMENT=="H"),] %>%
+  .[!(.$species=="IMG"),]
+
+### add steps: only do the above step for length measurements then do a seperate one for area 
 
 # graph 
 
-ggplot(calice_W_means, aes(x=Label, y=mean_W)) +
+ggplot(calice_area_means, aes(x=1:nrow(calice_W_means), y=mean_length, color=treatment)) +
   geom_point() +
-  labs(y= "Mean Calice Width", x = "Sample Name")
+  labs(y= "Mean Calice Width", x = "Sample Name") +
+  facet_wrap(~ species)
 
-ggplot(calice_W_means, aes(x=treatment, y=mean_W)) + 
-  geom_boxplot() +
-  facet_wrap(~ MEASUREMENT)
+ggplot(calice_H_means, aes(x=treatment, y=mean_length)) + 
+  geom_boxplot() + 
+  facet_wrap(~ species)
 
 ggsave("calice_w.jpg", plot = graph1, path = '~/Desktop')
