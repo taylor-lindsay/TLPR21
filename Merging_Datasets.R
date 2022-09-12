808e06e52bd5138614a4e50266c83ce16d5b0977
taylor-lindsay <taylor_lindsay@uri.edu>
2022-09-12 12:35:24 -0400

    new files for data analysis

--- Merging_Datasets.R
+++ Merging_Datasets.R
@@ -1,28 +1,17 @@
+
+
+# Packages & Data Import  -------------------------------------------------
+
 #Merging Datasets
 library(tidyverse)
-
-
-# merge morphology dataset with master ------------------------------------
 
 # Import data 
 raw_calice <- read.csv('~/Desktop/GITHUB/CF_2022/Transplants_Calice_Master.csv')
 raw_master <- read.csv('~/Desktop/GITHUB/TLPR21/Transplants_Raw_Master.csv')
 
-# Merge the two datasets by the label 
-merged <- merge(x= raw_master, y=raw_calice, by= 'Label', all=T)
-NAS <- merged[is.na(c(merged$Min)),]
-wrong_label <-NAS[(NAS$Airbrush_Date==""),]
-need_data <-NAS[!(NAS$Species=="PRACTICE" | NAS$Airbrush_Date=="MISSING"),]
-
-# save the file 
-write.csv(need_data, '~/Desktop/Need_Data.csv')
-
-
 # Cleaning morphology dataset ---------------------------------------------
 
-# original data set had all the samples starting with 0 as O so we need to replace those 
-
-# Clean benthic dataset
+# Change "O" to "0" in sample labels CALICE FILE
 raw_calice$Label <- raw_calice$Label %>% 
   gsub("OFAV_SP_OAZ24", "OFAV_SP_0AZ24", .)  %>%
   gsub("OFAV_SP_OAZ4", "OFAV_SP_0AZ4", .)  %>%
@@ -53,8 +42,7 @@ raw_calice$Label <- raw_calice$Label %>%
   gsub("OFRA_PP_2673_P1", "OFRA_PP_2GT3_P1", .)  %>%
   gsub("OFAV_PS_OHS4", "OFAV_PS_0HS4", .) 
 
-
-
+# Change "O" to "0" in sample labels MASTER FILE
 raw_master$Label <- raw_master$Label %>% 
   gsub("OFAV_SP_OHS2", "OFAV_SP_0HS2", .) %>%
   gsub("OFAV_PS_OHS2", "OFAV_PS_0HS2", .) %>%
@@ -65,7 +53,26 @@ raw_master$Label <- raw_master$Label %>%
 raw_master$Sample <- raw_master$Sample %>% 
   gsub("OHS2", "0HS2", .) 
   
-# rewrite the files 
+# Remove unneeded sample names from CALICE FILE 
+raw_calice$Label <- raw_calice
 
-write.csv(raw_calice, '~/Desktop/GITHUB/CF_2022/Transplants_Calice_Master.csv')
+# rewrite the files 
+write.csv(raw_calice, '~/Desktop/GITHUB/TLPR21/Transplants_Calice_Master.csv')
 write.csv(raw_master, '~/Desktop/GITHUB/TLPR21/Transplants_Raw_Master.csv')
+
+
+# merge morphology dataset with master ------------------------------------
+
+# Merge the two datasets by the label 
+merged <- merge(x= raw_master, y=raw_calice, by= 'Label', all=T)
+
+write.csv(merged, '~/Desktop/GITHUB/TLPR21/Transplants_Calices_Merged.csv')
+
+NAS <- merged[is.na(c(merged$Min)),]
+wrong_label <-NAS[(NAS$Airbrush_Date==""),]
+need_data <-NAS[!(NAS$Species=="PRACTICE" | NAS$Airbrush_Date=="MISSING"),]
+
+# save the file 
+write.csv(need_data, '~/Desktop/Need_Data.csv')
+
+
